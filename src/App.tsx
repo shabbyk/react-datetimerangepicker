5853df580021ccbdb18fb718a558e74c04f8f6f4
Shahbaaz Khan <shahbaaz.khan001@gmail.com>
2022-06-12 23:16:09 +0530

    - some styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,22 @@
+import React, { useEffect, useRef, useState } from "react";
+
 import { DateTime } from "luxon";
-import React, { useState } from "react";
 
 import "./App.css";
-import CalendarControls from "./components/CalendarControls/Index";
-import Calendar from "./components/Calendar/Index";
-
-function init() {
-  return {
-    year: +DateTime.now().toFormat("yyyy"),
-    month: +DateTime.now().toFormat("MM"),
-    monthName: DateTime.now().monthShort,
-  };
-}
+import CalendarLayout from "./components/CalendarLayout/Index";
 
 function App() {
-  const [currDateDetails, setCurrDate] = useState(init());
-  const [selectedDate, setSelectedDate] = useState(
-    DateTime.now().startOf("day")
-  );
+  const [showPopup, setShowPopup] = useState(false);
 
   return (
-    <div className="App">
-      <input type="text" name="dates" className="" />
-      <CalendarControls
-        month={currDateDetails.month}
-        year={currDateDetails.year}
-        monthName={currDateDetails.monthName}
-        navigate={setCurrDate}
+    <div className="date-range-picker">
+      <input
+        type="text"
+        name="dates"
+        className=""
+        onFocus={(e) => setShowPopup(true)}
       />
-      <Calendar
-        month={currDateDetails.month}
-        year={currDateDetails.year}
-        selectedDate={selectedDate}
-        selectDate={setSelectedDate}
-      />
+      {showPopup && <CalendarLayout closeFn={setShowPopup} />}
     </div>
   );
 }
---
+++ src/components/CalendarLayout/Index.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+
+import { DateTime } from "luxon";
+
+import Calendar from "../Calendar/Index";
+import CalendarControls from "../CalendarControls/Index";
+
+type CalendarLayoutProps = {
+  closeFn: (show: boolean) => any;
+};
+
+function init() {
+  return {
+    year: +DateTime.now().toFormat("yyyy"),
+    month: +DateTime.now().toFormat("MM"),
+    monthName: DateTime.now().monthShort,
+  };
+}
+
+function handleOutsideClick(ref: any, e: any, closeFn: (show: boolean) => any) {
+  debugger;
+  if (ref.current && !ref.current.contains(e.target)) {
+    closeFn(false);
+  }
+}
+
+function CalendarLayout(props: CalendarLayoutProps) {
+  const [currDateDetails, setCurrDate] = useState(init());
+  const [selectedDate, setSelectedDate] = useState(
+    DateTime.now().startOf("day")
+  );
+  const { innerWidth, innerHeight } = window;
+  const appRef = useRef(null);
+  useEffect(() => {
+    document.addEventListener("mousedown", (e) =>
+      handleOutsideClick(appRef, e, props.closeFn)
+    );
+    return () =>
+      document.removeEventListener("mousedown", (e) =>
+        handleOutsideClick(appRef, e, props.closeFn)
+      );
+  });
+  return (
+    <div
+      ref={appRef}
+      style={{
+        width: 200,
+      }}
+    >
+      <CalendarControls
+        month={currDateDetails.month}
+        year={currDateDetails.year}
+        monthName={currDateDetails.monthName}
+        navigate={setCurrDate}
+      />
+      <Calendar
+        month={currDateDetails.month}
+        year={currDateDetails.year}
+        selectedDate={selectedDate}
+        selectDate={setSelectedDate}
+      />
+    </div>
+  );
+}
+
+export default CalendarLayout;
