b00fa3b4a30cf4f52e985f96db5a1cb46d7c80f9
shahbaaz <shahbaaz.khan001@gmail.com>
2022-06-23 09:23:10 +0530

    - updated new package in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,11 +4,12 @@ import ReactDateTimeRangePicker from '@shabbyk/react-datetimerangepicker';
 
 import '@shabbyk/react-datetimerangepicker/ReactDateTimeRangePicker.css';
 import './App.css';
+import { DateTime } from 'luxon';
 
 function App() {
   return (
     <div className="App">
-      <ReactDateTimeRangePicker dateFormat='dd/MM/yyyy hh:mm:ss' width={200} dateRangePicker/>
+      <ReactDateTimeRangePicker dateFormat='dd/MM/yyyy hh:mm:ss' width={200} dateRangePicker selectedDate={DateTime.now()}/>
     </div>
   );
 }
